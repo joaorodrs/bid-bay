d7657fdf719918b7a5f99d2e520ffc4157ab14cc
Joao Paulo Alencar Rodrigues <joao.paulo@inovan.do>
2024-02-24 14:28:02 -0300

    feat(register): adds prisma and database configuration and register action

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,9 +2,41 @@
 
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import { User } from "@prisma/client";
+
+import { db } from "./database";
 
 export async function authenticate(body: string) {
-  cookies().set("currentUser", body);
+  const parsedBody: User = JSON.parse(body);
+
+  await db.$connect();
+
+  const newUser = await db.user.create({
+    data: {
+      name: parsedBody.name,
+      email: parsedBody.email,
+    },
+  });
+
+  cookies().set("currentUser", JSON.stringify(newUser));
+  await db.$disconnect();
 
   redirect("/");
+}
+
+export async function register(body: string) {
+  const parsedBody: User = JSON.parse(body);
+
+  await db.$connect();
+
+  await db.user.create({
+    data: {
+      name: parsedBody.name,
+      email: parsedBody.email,
+    },
+  });
+
+  await db.$disconnect();
+
+  redirect("/login");
 }
---
+++ lib/database.ts
@@ -0,0 +1,12 @@
+/* eslint no-var: off */
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient;
+}
+
+const client = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+
+export const db = client;
