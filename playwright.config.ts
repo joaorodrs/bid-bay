5a46bc0182647e373d62cd1c2e20f15190799788
Joao Paulo Alencar Rodrigues <joao.paulo@inovan.do>
2024-02-24 23:17:19 -0300

    feat(register): adds register validation and testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,5 +73,6 @@ export default defineConfig({
     command: "npm run dev:test",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
+    ignoreHTTPSErrors: true,
   },
 });
